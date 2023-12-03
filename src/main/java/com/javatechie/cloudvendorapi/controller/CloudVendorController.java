6bc72ac3152a00649c0f2ffc6e0679f6f2e46dc0
Kushla95 <das.kushal95@gmail.com>
2023-12-04 00:56:48 +0530

    Commit 2 for Post request of vendor details

--- src/main/java/com/javatechie/cloudvendorapi/controller/CloudVendorController.java
+++ src/main/java/com/javatechie/cloudvendorapi/controller/CloudVendorController.java
@@ -2,6 +2,8 @@ package com.javatechie.cloudvendorapi.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,27 +12,33 @@ import com.javatechie.cloudvendorapi.model.CloudVendor;
 @RestController
 @RequestMapping("/cloudvendor")
 public class CloudVendorController {
-	
-	
+
+	CloudVendor cloudVendor;
 
 	@GetMapping("{vendorId}")
-    public CloudVendor getVendorById(@PathVariable String vendorId) {
-       
-        return new CloudVendor("C1","Vendor 1","Address One","xxxxx");
-    }
+	public CloudVendor getVendorById(@PathVariable String vendorId) {
 
-	/*
-	 * @PostMapping public CloudVendor createVendor(@RequestBody CloudVendor
-	 * cloudVendor) { // Implement logic to create a new vendor // For example, you
-	 * can use a service to save data to a database return your logic to create a
-	 * new vendor ; }
-	 * 
-	 * @PutMapping("/{vendorId}") public CloudVendor updateVendor(@PathVariable
-	 * String vendorId, @RequestBody CloudVendor cloudVendor) { // Implement logic
-	 * to update an existing vendor return your logic to update a vendor ; }
-	 * 
-	 * @DeleteMapping("/{vendorId}") public void deleteVendor(@PathVariable String
-	 * vendorId) { // Implement logic to delete an existing vendor // For example,
-	 * you can use a service to delete data from a database }
-	 */
+		return cloudVendor;
+		// new CloudVendor("C1","Vendor 1","Address One","xxxxx");
+	}
+
+	
+	  @PostMapping 
+	  public String createCloudVendorDetails(@RequestBody CloudVendor cloudVendor)
+	  {
+		  this.cloudVendor=cloudVendor;
+		  return "Cloud Vendor Created Successfully"; 
+		  
+	  }
+	  
+		/*
+		 * @PutMapping("/{vendorId}") public CloudVendor updateVendor(@PathVariable
+		 * String vendorId, @RequestBody CloudVendor cloudVendor) { // Implement logic
+		 * to update an existing vendor return your logic to update a vendor ; }
+		 * 
+		 * @DeleteMapping("/{vendorId}") public void deleteVendor(@PathVariable String
+		 * vendorId) { // Implement logic to delete an existing vendor // For example,
+		 * you can use a service to delete data from a database }
+		 */
+	 
 }
